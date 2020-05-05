e35a4ce14e18d8baa31be83dbdb99ee94b23b81f
matthewkellison <58009482+matthewkellison@users.noreply.github.com>
2020-05-05 16:32:50 -0400

    Update settings.py

--- CoreBasicsLMS/settings.py
+++ CoreBasicsLMS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'p#1i^kxibtzw!=8*=bj_^qus_a50s8s3i#+2eeb%y9@jhmsgi&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
